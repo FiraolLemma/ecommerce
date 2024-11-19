df30f96aa0e0f1ae1c5da56522bffa96c21dc9e2
fraol lemma <fraollemma0506@gmail.com>
2024-11-19 10:14:25 +0300

    second eco commit

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def store(request):
-     context = {}
+     products = Product.objects.all()
+     context = {'products':products}
      return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -13,4 +14,4 @@ def cart(request):
 
 def checkout(request):
       context = {}
-      return render(request, 'store/checkout.html', context) 
+      return render(request, 'store/checkout.html', context)  
